c9943419681a60f9c2bef76c55c1ac9f1e44f738
Anthony-Jerez <ajerez450@gmail.com>
2021-07-28 20:24:35 +0000

    updatefive

--- app.py
+++ app.py
@@ -4,46 +4,38 @@ from flask import Flask, render_template, request, url_for
 from datetime import datetime
 from model import determine_air_quality
 import os
-
 # -- Initialization section --
 app = Flask(__name__)
-
-
 # -- Routes section --
-
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html", time = datetime.now())
-
-
-
 @app.route('/AirPollution')
 def AirPollution():
     return render_template("AirPollution.html", time = datetime.now())
-
 @app.route('/WaterPollution')
 def WaterPollution():
     return render_template("WaterPollution.html", time = datetime.now())
-
 @app.route('/LandPollution')
 def LandPollution():
     return render_template("LandPollution.html", time = datetime.now())
-
 @app.route('/WaysToHelp')
 def WaysToHelp():
     return render_template("WaysToHelp.html", time = datetime.now())
-
 @app.route('/Survey')
 def Survey():
     return render_template("Survey.html", time = datetime.now())
-
 @app.route("/SurveyResults", methods = ["GET", "POST"])
 def getSurveyResults():     
-
-    user_borough = request.form["borough"]
-
-    air_quality = determine_air_quality(user_borough)
-
+    postal = request.form["postal"]
+    country = request.form["country"]
+    air_quality = determine_air_quality(postal, country)
     return render_template("SurveyResults.html", time = datetime.now(), airquality = air_quality)
+    
+#user_borough = request.form["borough"]
+
+#air_quality = determine_air_quality(user_borough)
+
+#return render_template("SurveyResults.html", time = datetime.now(), airquality = air_quality)
     
--- model.py
+++ model.py
@@ -1,17 +1,30 @@
-def determine_air_quality(userLocation):
+import requests
+url = "https://api.ambeedata.com/latest/by-postal-code"
+def determine_air_quality(postal, country):
+    querystring = {"postalCode": postal,"countryCode": country}
+    headers = {
+         'x-api-key': "a360a0c90a4df99f938befb309e16ff0b517b06562b8743e87f71cf001bce5f4",
+         'Content-type': "application/json"
+         }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    return response.text
+#print(response.text)
 
-    airqualities = {
-        "queens": "123",
-        "manhattan": "456",
-        "bronx": "789",
-        "brooklyn": "10",
-        "staten island": "11" 
-    }
-    for location in airqualities:
-        if userLocation.lower() == location: 
-            return airqualities[location]
-        else: 
-            return "Invalid Response"
+
+#def determine_air_quality(userLocation):
+
+    #airqualities = {
+       # "queens": "123",
+        #"manhattan": "456",
+        #"bronx": "789",
+        #"brooklyn": "10",
+       # "staten island": "11" 
+    #}
+    #for location in airqualities:
+        #if userLocation.lower() == location: 
+            #return airqualities[location]
+        #else: 
+            #return "Invalid Response"
 
 
 
