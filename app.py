a12431e5892a2a358165ff32503d2843e1e51e77
Anthony-Jerez <ajerez450@gmail.com>
2021-07-28 13:39:10 +0000

    update2

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from datetime import datetime
-# from model import getImageUrlFrom
+from model import getImageUrlFrom
 import os
 
 # -- Initialization section --
@@ -10,7 +10,30 @@ app = Flask(__name__)
 
 
 # -- Routes section --
+
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html", time = datetime.now())
+
+
+
+@app.route('/AirPollution')
+def AirPollution():
+    return render_template("AirPollution.html", time = datetime.now())
+
+@app.route('/WaterPollution')
+def WaterPollution():
+    return render_template("WaterPollution.html", time = datetime.now())
+
+@app.route('/LandPollution')
+def LandPollution():
+    return render_template("LandPollution.html", time = datetime.now())
+
+@app.route('/WaysToHelp')
+def WaysToHelp():
+    return render_template("WaysToHelp.html", time = datetime.now())
+
+@app.route('/Survey')
+def Survey():
+    return render_template("Survey.html", time = datetime.now())
